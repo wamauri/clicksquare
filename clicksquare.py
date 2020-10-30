796adceaa8efa89b98b21b4cc807713a2c72b7af
Amauri Jesus <amaurisantospro@gmail.com>
2020-10-30 20:00:50 -0300

    Game screen created

--- clicksquare.py
+++ clicksquare.py
@@ -2,5 +2,21 @@ import pygame
 
 
 pygame.init()
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
 
+finished = False
+while not finished:
+    # Updates the drawing on the screen
+    pygame.display.update()
+
+    # Check mouse events here
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
+# End the game window
+pygame.display.quit()
+# End the pygame
 pygame.quit()
