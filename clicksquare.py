fca376fe0488678f5f8cdbd647a6fb5ab7d3d7d3
Amauri Jesus <amaurisantospro@gmail.com>
2020-11-01 21:52:21 -0300

    Red squares on screen

--- clicksquare.py
+++ clicksquare.py
@@ -2,35 +2,35 @@ import pygame
 import random
 
 
-class Squares():
+class Squares:
+
     def __init__(self, screen):
         self.screen = screen
-        self.width = 30
-        self.height = 30
+        self.width = 20
+        self.height = 20
         self.x = random.randint(0, screen_width - self.width)
         self.y = random.randint(0, screen_height - self.height)
         self.area = pygame.Rect(self.x, self.y, self.width, self.height)
-        self.color = (random.randint(20, 255), random.randint(20, 255), random.randint(20, 255))
+        # self.color = (random.randint(20, 255), random.randint(20, 255), random.randint(20, 255))
+        self.color = (random.randint(255, 255), random.randint(0, 0), random.randint(0, 0))
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, self.area)
 
 
-screen_width = 800
-screen_height = 600
-initial_squares = 30
+# screen_width = 800
+# screen_height = 600
+screen_width = 1200
+screen_height = 700
+initial_squares = 500
 black = (0, 0, 0)
 white = (255, 255, 255)
+initial_time = 10
 
 pygame.init()
 pygame.display.set_caption("Click Square")
 screen = pygame.display.set_mode((screen_width, screen_height))
-
-
-yellow = (255, 255, 0)
-red = (255, 0, 0)
 clock = pygame.time.Clock()
-
 
 
 finished = False
@@ -51,7 +51,7 @@ while not finished:
     # Screen color
     screen.fill((51, 51, 51))
     # Sets 50 screen update per second
-    clock.tick(50)
+    clock.tick(10)
 
 # End the game window
 pygame.display.quit()
